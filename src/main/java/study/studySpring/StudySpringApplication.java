9b5194c2b7de8667448187d5af54eb2c512737ef
Newbiedevelope <rbtjr20313@gmail.com>
2026-01-02 18:47:41 +0900

    Controller 만들고 spring 컨테이너의 MVC 패턴 지원 이해하기

--- src/main/java/study/studySpring/StudySpringApplication.java
+++ src/main/java/study/studySpring/StudySpringApplication.java
@@ -1,4 +1,4 @@
-package study.study_spring;
+package study.studySpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/study/studySpring/StudySpringApplicationTests.java
+++ src/test/java/study/studySpring/StudySpringApplicationTests.java
@@ -1,4 +1,4 @@
-package study.study_spring;
+package study.studySpring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
