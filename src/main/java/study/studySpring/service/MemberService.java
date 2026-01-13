b55b71033143b0c771e831761324aaddb12f9ec4
Newbiedevelope <rbtjr20313@gmail.com>
2026-01-14 05:29:01 +0900

    회원 서비스 구현

---
+++ src/main/java/study/studySpring/service/MemberService.java
@@ -0,0 +1,40 @@
+package study.studySpring.service;
+
+import study.studySpring.domain.Member;
+import study.studySpring.repository.MemberRepository;
+import study.studySpring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입 메소드
+    public long join(Member member){
+        // 동명의 회원은 X(중복)
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        // 간단한 로직으로 회원 가입 시 아이디 반환
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+
+}
