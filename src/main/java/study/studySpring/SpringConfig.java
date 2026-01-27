00826bda645956c31d94448fce575c286a071643
Newbiedevelope <rbtjr20313@gmail.com>
2026-01-28 00:12:29 +0900

    순수 JDBC 구현해보기

--- src/main/java/study/studySpring/SpringConfig.java
+++ src/main/java/study/studySpring/SpringConfig.java
@@ -1,13 +1,24 @@
 package study.studySpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import study.studySpring.repository.JdbcMemberRepository;
 import study.studySpring.repository.MemberRepository;
 import study.studySpring.repository.MemoryMemberRepository;
 import study.studySpring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    private SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
